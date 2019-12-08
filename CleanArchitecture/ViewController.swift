41aeae52b501e40288c6883eafb85ee22f13bad7
Dong Uk Kim <sanich8355@daum.net>
2019-12-08 15:47:18 +0900

    Initial Commit

---
+++ CleanArchitecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanArchitecture
+//
+//  Created by 김동욱 on 08/12/2019.
+//  Copyright © 2019 sanichdaniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CleanArchitectureTests/CleanArchitectureTests.swift
@@ -0,0 +1,34 @@
+//
+//  CleanArchitectureTests.swift
+//  CleanArchitectureTests
+//
+//  Created by 김동욱 on 08/12/2019.
+//  Copyright © 2019 sanichdaniel. All rights reserved.
+//
+
+import XCTest
+@testable import CleanArchitecture
+
+class CleanArchitectureTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
